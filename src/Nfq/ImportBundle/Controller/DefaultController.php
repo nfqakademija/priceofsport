fd6fe953472a1d54f2d2477501b7b28364f00f5b
DariusPa <dariuk.p@gmail.com>
2016-03-23 13:32:30 +0200

    Created bundles

---
+++ src/Nfq/ImportBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nfq\ImportBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('NfqImportBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Nfq/ImportBundle/NfqImportBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nfq\ImportBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NfqImportBundle extends Bundle
+{
+}
