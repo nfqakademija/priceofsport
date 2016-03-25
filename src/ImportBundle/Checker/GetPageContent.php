c7591af90792779f22a45b3a78beeb4c761c67d8
Tomas Mataitis <mataitis.tomas@gmail.com>
2016-03-25 15:38:35 +0200

    Added GetPageContent.php URL checker

---
+++ src/ImportBundle/Checker/GetPageContent.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace  ImportBundle\Checker;
+
+use GuzzleHttp\Client;
+
+/**
+ * Created by PhpStorm.
+ * User: tomas
+ * Date: 3/24/16
+ * Time: 7:13 PM
+ */
+
+class GetPageContent {
+
+    public function getContent($url) {
+        $file = $url;
+        $file_headers = @get_headers($file);
+        if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+            echo "This page doesn't exists";
+            return null;
+        }
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $out = curl_exec($ch);
+
+        $out = str_replace("\r", "", $out);
+
+        $headers_end = strpos($out, "\n\n");
+        if( $headers_end !== false ) {
+            $out = substr($out, 0, $headers_end);
+        }
+
+        $headers = explode("\n", $out);
+
+        foreach($headers as $header) {
+            if( substr($header, 0, 10) == "Location: " ) {
+                $url = substr($header, 10);
+                break;
+            }
+        }
+
+        curl_close($ch);
+
+        $html = file_get_contents($url);
+
+        return $html;
+
+    }
+
+}
+
