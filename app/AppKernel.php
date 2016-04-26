692cf1fe79fa1e3b240d644500b65339f0c2f2ac
Mindaugas Galvanauskas <m.galvanauskas@gmail.com>
2016-04-26 15:58:45 +0300

    Create frontend category list

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new FrontBundle\FrontBundle(),
             new ImportBundle\ImportBundle(),
             new ProcessBundle\ProcessBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
