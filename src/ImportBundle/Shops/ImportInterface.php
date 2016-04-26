475ef65c8203d07296f290d6145070c227eed7a0
DariusPa <dariuk.p@gmail.com>
2016-04-26 16:44:59 +0300

    Updated interface file

--- src/ImportBundle/Shops/ImportInterface.php
+++ src/ImportBundle/Shops/ImportInterface.php
@@ -5,6 +5,14 @@ namespace ImportBundle\Shops;
 interface ImportInterface
 {
     public function getCategories($shopLink);
-    public function getPages($categoryLink);
+    public function getCategoryName($shopLink);
     public function getLinks($categoryLink);
+    public function getPages($categoryLink);
+    public function getPaginationPrefix($shopId, $page);
+    public function mapCategoryName($categoryName);
+
+    public function getImage($pageLink);
+    public function getDescription($pageLink);
+    public function getPrice($pageLink);
+    public function getTitle($pageLink);
 }
